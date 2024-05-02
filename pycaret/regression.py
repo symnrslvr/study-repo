629f8b707e33b6afc1204c536e0a009c401a9de9
Seymanur Selver <seyma@gohm.com.tr>
2024-05-02 19:55:10 +0300

    adding

--- pycaret/regression.py
+++ pycaret/regression.py
@@ -8,3 +8,17 @@ evaluate_model(best)
 plot_model(best, plot = 'residuals')
 plot_model(best, plot = 'feature')
 predict_model(best)
+
+
+predictions = predict_model(best, data=data)
+predictions.head()
+
+
+
+save_model(best, 'my_best_pipeline')
+
+
+
+loaded_model = load_model('my_best_pipeline')
+print(loaded_model)
+
