ed500e930394fa1f3589e8a480bb0e57648debcf
Seymanur Selver <symnrslvr@gmail.com>
2024-05-03 01:00:41 +0300

    edited repo

--- pycaret/pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# If your dataset is a local CSV file
-data = pd.read_csv('/home/seyma/study-repo/pycaret/data2.csv')
-
-# If your dataset is available at a URL
-# data = pd.read_csv('http://url_to_your_data/data2.csv')
-
-print(data.head())  # This will display the first few rows of your dataset
--- pycaret/regression.py
+++ pycaret/regression.py
@@ -2,7 +2,7 @@ from pycaret.datasets import get_data
 data = get_data('data2')
 
 from pycaret.regression import *
-s = setup(data, target = 'charges', session_id = 123)
+s = setup(data, target = 'Efficiency', session_id = 123)
 best = compare_models()
 evaluate_model(best)
 plot_model(best, plot = 'residuals')
